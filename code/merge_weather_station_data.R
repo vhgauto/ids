9ea2f8b5c25752e6591af824706ae3c9e78090f6
vhgauto <victor.gauto@outlook.com>
2022-10-13 21:56:25 -0300

    Agrego índice Z a los datos de las estaciones meteorológicas

--- code/merge_weather_station_data.R
+++ code/merge_weather_station_data.R
@@ -17,13 +17,18 @@ lat_long_prcp <- inner_join(prcp_data, station_data, by = "id") %>%
   # remuevo años inicial y final, excepto 2022
   filter((year != first_year & year != last_year) | year == 2022) %>% 
   group_by(latitude, longitude, year) %>% 
-  summarise(mean_prcp = mean(prcp)) # %>% 
-  # summarise(n = n())
+  summarise(mean_prcp = mean(prcp), .groups = "drop")
 
-# lat_long_prcp %>% 
-#   ggplot(aes(x = n)) +
-#   geom_histogram() +
-#   theme_classic()
-# 
-# lat_long_prcp %>% 
-#   filter(n >= 100)
+this_year <- lat_long_prcp %>% 
+  filter(year == 2022) %>% 
+  select(-year)
+
+
+inner_join(lat_long_prcp, this_year, by = c("latitude", "longitude")) %>% 
+  rename(all_years = mean_prcp.x, this_year = mean_prcp.y) %>% 
+  group_by(latitude, longitude) %>% 
+  summarise(# z-score
+         z_score = min(this_year) - mean(all_years)/sd(all_years),
+         n = n(), .groups = "drop") %>% 
+  # al menos 50 años de datos
+  filter(n >= 50)
